e1bb89ccf06fb2f4bc95459f6ba56de7c56f8527
Mark Godek <markgodek@gmail.com>
2025-05-20 15:08:01 -0400

    updated output to include title and date in each entry because these should be on each row in SQL

--- scrapeNHKnews.py
+++ scrapeNHKnews.py
@@ -4,6 +4,8 @@ import neologdn
 import re
 from urllib.parse import urljoin
 
+debug = True
+
 #site map for news articles of the last 3 days
 #probably a smarter way to pull the links
 #https://www3.nhk.or.jp/news/sitemap-news-flow-3days.xml
@@ -44,33 +46,52 @@ def extract_text(url):
             print("No recognizable content section found.")
             return []
 
+    article_title = ''
     content = []
+
+    time_tag = section_to_use.find('time')
+    article_datetime = time_tag.get('datetime') if time_tag and time_tag.has_attr('datetime') else None
 
     for tag in section_to_use.find_all(True):  # All tags
         # Only capture direct text (not from children)
         tag_text = ''.join(tag.find_all(string=True, recursive=False)).strip()
         normalized_text = neologdn.normalize(tag_text)
 
+        # if the tag has text and was normalized, get it's class and parent class and store them as strings
         if normalized_text:
-            parent_with_class = tag.find_parent(attrs={'class': True})
-            class_name = ' '.join(parent_with_class.get('class')) if parent_with_class else None
+            class_ = ' '.join(tag.get('class')) if tag.has_attr('class') else None
+
+            if (parent := tag.find_parent(attrs={'class': True})):
+                parent_class = ' '.join(parent.get('class', []))
+            else:
+                parent_class = None
+            # if the text has a title tag, store that as the title for all sections
+            if parent_class == 'content--title' or class_ == 'contentTitle':
+                article_title = normalized_text
+
             content.append({
+                'article_title': article_title,
+                'date': article_datetime,
                 'text': normalized_text,
                 'tag': tag.name,
-                'class': ' '.join(tag.get('class')) if tag.has_attr('class') else None,
-                'parent_class': class_name
+                'class': class_,
+                'parent_class': parent_class,
+                'url': url
             })
-            # print(f"Text: {text}\nTag: <{tag.name}>\nClass: {tag.get('class')}\nParent class: {class_name}\n---")
+            if debug == True:
+                print(f"Article_title: {article_title}\nDate: {article_datetime}\n"
+                      f"Text: {normalized_text}\nTag: <{tag.name}>\n"
+                      f"Class: {class_}\nParent class: {parent_class}\n---")
     return content
 
 homepage = 'https://www3.nhk.or.jp/news/'
 articles = get_articles(homepage)
 
+# pull the text from all articles
 for article in articles:
     print(article[1])
     text = extract_text(article[1])
-    print(text)
-
+    #print(text)
 
 #extract only the kanji
 if False:
